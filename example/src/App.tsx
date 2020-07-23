bd80add852473c783bb7056c0d5d29b97bd86bf2
András Szücs <andrew0szucs@gmail.com>
2020-07-24 00:23:12 +0200

    Implement TikTok in example, remove unnecessary files

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { TikTok } from 'react-tiktok';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>react-tiktok Example:</h1>
+      <TikTok url={`https://www.tiktok.com/@scout2015/video/6718335390845095173`} />
     </div>
   );
 }
