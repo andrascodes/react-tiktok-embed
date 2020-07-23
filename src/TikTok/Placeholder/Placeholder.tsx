5637d1f4f86aad59db8aff2bfd41e75e997c4825
András Szücs <andrew0szucs@gmail.com>
2020-07-24 00:21:06 +0200

    Add TikTok component

---
+++ src/TikTok/Placeholder/Placeholder.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// const TIKTOK_DISCOVER_URL = `https://www.tiktok.com/foryou`;
+
+export default function Placeholder() {
+  // { text, show, href }
+  return (
+    <div></div>
+    // <div
+    //   style={{ display: show ? 'flex' : 'none' }}
+    //   className={styles.placeholder}
+    // >
+    //   <div className={styles.top}>Loading...</div>
+    //   <div className={styles.bottom}>
+    //     <div className={styles.captionContainer}>
+    //       <Skeleton paragraph={{ rows: 1, width: '100%' }} />
+    //     </div>
+    //     <div className={styles.discoverMoreButtonContainer}>
+    //       <a href={TIKTOK_DISCOVER_URL}>Discover more on TikTok</a>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
